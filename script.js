fdbe3eefd367819dca849fabb0b99cb3baf1337f
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-10 16:46:08 +0100

    addedboolean variables

--- script.js
+++ script.js
@@ -6,10 +6,47 @@ const accMail = ["ciao@gmail.com" , "oggi@hotmail.com" , "ieri@yahoo.com"];
 
 const userMail = prompt("inserisci la tua email");
 
+let aut = false;
+let notAut = false;
+
 for ( i = 0 ; i < accMail.length ; i++){
     currentMail = accMail[i];
 
     if(userMail === currentMail){
-      console.log("mail corretta");
-    } 
+      aut = true;
+    } else {
+        notAut = true;
+    }
+}
+
+const autMessage = "email corretta";
+const unautMessage = "non sei autorizzato";
+
+if (aut == true){
+    console.log("email corretta");
+} else {
+    console.log("non sei autorizzato");
+}
+
+
+const displayElement = document.getElementById("list");
+
+
+ 
+
+
+// Genero numero random per l'utente
+// Genero numero random per il computer
+// Li confronto e stabilisco il vincitore
+
+const userNumber = Math.random();
+const compNumber = Math.random();
+
+console.log(userNumber);
+console.log(compNumber);
+
+if(userNumber > compNumber){
+    console.log("L'utente ha vinto!");
+} else {
+    console.log("Il computer ha vinto!");
 }
